a3de30f8777c19f93c78781bfc8883945a61a615
SCHWARZ <brett.schwarz@gmail.com>
2017-03-21 16:32:30 -0700

    Adds angular routing

--- scripts/application.js
+++ scripts/application.js
@@ -1,1 +1,25 @@
 var myApp = angular.module('mobileWebApp', ['ngRoute','ui.bootstrap']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  
+    $routeProvider.
+    when('/phones', {
+        templateUrl: 'views/phones.html',
+        controller: 'PhoneCtrl'
+    }).
+    when('/plans', {
+        templateUrl: 'views/plans.html',
+        controller: 'PlansCtrl'
+    }).
+    when('/cart', {
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl'
+    }).
+    when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+}]);
