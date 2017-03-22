d702a3601a4a4fca6746e2d7829fbd5c75e200d3
SCHWARZ <brett.schwarz@gmail.com>
2017-03-21 17:03:26 -0700

    Adds carousel to Home

--- scripts/application.js
+++ scripts/application.js
@@ -24,6 +24,39 @@ myApp.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-myApp.controller('HomeCtrl',function($scope) {
+myApp.controller('HomeCtrl',function($log, $scope) {
+    $log.debug('initializing HomeCtrl');
     $scope.interval = 5000; // carousel animation interval
+    // $scope.active = 0;
+    //carousel slide info
+	$scope.slides = [
+	    {
+	    	id: 0,
+	        img: "images/apple-iphone7.jpg",
+	        url: "#/phones",
+	        text: "New Shiny iPhone 7",
+	        title: "iPhone 7"
+	    },
+	    {
+	    	id: 1,
+	        img: "images/lg-v20-titan.jpg",
+	        url: "#/phones",
+	        text: "New Shiny LG V20",
+	        title: "LG V20"
+	    },
+	    {
+	    	id: 2,
+	        img: "images/samsung-galaxys7edge.jpg",
+	        url: "#/phones",
+	        text: "New Shiny Samsung Galaxy S7 Edge",
+	        title: "Samsung Galaxy S7 Edge"
+	    },
+	    {
+	    	id: 3,
+	        img: "images/lg-g6-black.jpg",
+	        url: "#/phones",
+	        text: "New Shiny LG G6",
+	        title: "LG G6"
+	    }
+	];
 });
