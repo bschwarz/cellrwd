ec5a2f906ac8f51e04cb52a25aa68dd83416f0bc
SCHWARZ <brett.schwarz@gmail.com>
2017-05-02 22:02:29 -0700

    Adds support for string replace, for 2 targets prod and dev

--- gruntfile.js
+++ gruntfile.js
@@ -45,15 +45,47 @@ module.exports = function(grunt) {
                     'scripts/application.min.js':'scripts/application.annotated.js'
                 }
             }
-          }
+          },
+          'string-replace': {
+                dev: {
+                    files: {
+                      'index.html': 'index.tpl.html'
+                    },
+                    options: {
+                      replacements: [
+                      {
+                        pattern: '<script src="APPLICATION">',
+                        replacement: '<script src="scripts/application.js">'
+                      }
+                      ]
+                    }
+                },
+                prod: {
+                    files: {
+                      'index.html': 'index.tpl.html'
+                    },
+                    options: {
+                      replacements: [
+                      {
+                        pattern: '<script src="APPLICATION">',
+                        replacement: '<script src="scripts/application.min.js">'
+                      }
+                      ]
+                    }
+                }
+            }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-string-replace');
 
-    grunt.registerTask('build', ['cssmin', 'ngAnnotate', 'uglify']);
+    var target = grunt.option('target') || 'dev';
+    grunt.registerTask('build', ['cssmin', 'ngAnnotate', 'uglify', 'string-replace:' + target]);
     grunt.registerTask('server', ['connect']);
     grunt.registerTask('test', ['build', 'karma']);
 };
